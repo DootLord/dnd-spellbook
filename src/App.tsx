51869d1612485e2256e96724084590799fdd24c8
DootLord <shaunoman840@gmail.com>
2025-10-27 22:08:03 +0000

    Added filter system

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
 import './App.css'
+import { useEffect, useMemo, useState } from 'react'
 import spellsJSON from '../spells.json'
 import CardGrid from './components/card-grid/CardGrid'
+import CardSearch from './components/search/card-search';
 
 export type spellsJSONDataType = typeof spellsJSON[keyof typeof spellsJSON];
 
 function App() {
   const spellsJSONKeys = Object.keys(spellsJSON);
-
-  // Get ALL of the spell data using spellsJSONKeys
-  let cardData = [
+  const allSpells = [
     ...spellsJSONKeys.map((key) => (spellsJSON as any)[key])
-  ]
+  ];
+  const [searchText, setSearchText] = useState("");
+  const [currentSpells, setCurrentSpells] = useState<spellsJSONDataType[]>([]);
+
+  useEffect(() => {
+    if (searchText.trim() === "") {
+      setCurrentSpells(allSpells);
+      return;
+    }
+
+    const filteredSpells = [ //? Learning note: useMemo means we're only using it once!
+      ...spellsJSONKeys.map((key) => (spellsJSON as any)[key])
+    ].filter((spell) => {
+      return spell.name.toLowerCase().includes(searchText.toLowerCase())
+    });
+
+    setCurrentSpells(filteredSpells);
+  }, [searchText]);
+
 
   return (
     <>
-      <CardGrid columns={4} cards={cardData}></CardGrid>
+      <CardSearch onSearchChange={setSearchText} />
+      <CardGrid columns={4} cards={currentSpells}></CardGrid>
     </>
   )
 }
